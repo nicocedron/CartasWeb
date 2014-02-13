e1fdb78d6ed0e3ecd7c42c564d7921b330af6e26
FabianVeliz <velizfabianhoracio>
2014-02-13 14:57:45 -0300

    Correcciones index

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
 
 
 function Block(){
-	
+
 	$('#content').BlocksIt({
 		numOfCol: Math.round($('#content').width()/294),
 		offsetX: 8,
@@ -24,3 +24,30 @@ function IncrementBlocks(){
 	$('#content').append(content);
 }
 */
+
+$(document).on('ready',slide);
+
+function slide(){
+
+	var $curr = $(".boxSlide:first");
+	$curr.fadeIn();
+
+  $('#next').on('click',function(){
+    if($curr.next('.boxSlide').html()==undefined)
+      return;
+
+    $curr = $curr.next('.boxSlide');
+    $( ".boxSlide" ).fadeOut();
+  	$curr.fadeIn();
+  });
+
+  $('#prev').on('click',function(){
+        if($curr.prev('.boxSlide').html()==undefined)
+      return;
+
+    $curr = $curr.prev('.boxSlide');
+    $( ".boxSlide" ).fadeOut();
+  $curr.fadeIn();
+  });
+
+}
