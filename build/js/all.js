179f2ab739171fa2a514a3aa8ae676a38bf0c46f
Nicolas Cedron <josenicolascedron@gmail.com>
2014-03-25 01:54:09 -0300

    Added Page for card expanded

--- build/js/all.js
+++ build/js/all.js
@@ -1,7 +1,59 @@
-$(document).on('ready',function(){
+var SimulateTouch=true;
+
+
+
+
+
+var card={
+	open:true,
+	show:function(){
+
+		if(!card.open)
+			return;
+
+		$('.subheader,#comparteList').hide();
+		$('.swiper-pages').css('top','45px');
+		$('.cardExpanded').show();
+
+		$('.header .left .logo').hide();
+		$('.header .left .back').show()
+			.one('click',back)
+			.one('click',function(){
+
+				$('.subheader,#comparteList').show();
+				$('.swiper-pages').css('top','');
+				$('.cardExpanded').hide();
+				resetPage(0);
+			});
+
+		resetPage(0);
+
+
+	}
+
+};
+
+function resetPage(key){
+		PAGE[key].reInit();
+		PAGE[key].resizeFix();
+		PAGE[key].setWrapperTranslate(0,0);
+}
+function back(){
+		$('.header .left .logo').show();
+		$('.header .left .back').hide();
+}
+
+function home(){
 	
 	pages = $('.swiper-pages').swiper({
 		resistance:'100%',
+		simulateTouch:SimulateTouch,
+		onTouchMove:function(){
+			card.open=false;
+		},		
+		onTouchEnd:function(){
+			setTimeout(function(){card.open=true;},500);
+		},
 		onSlideChangeStart:function(slide){
 			var active=slide.activeIndex;
 			$('ul.nav li').removeClass('active').eq(active).addClass('active');
@@ -16,13 +68,20 @@ $(document).on('ready',function(){
 	});
 
 	//Scroll Containers
-	var PAGE=[];
+	PAGE=[];
 	$('.scroll-container').each(function(i){
-		console.log(i);
+		
 		PAGE[i]= $(this).swiper({
 			mode:'vertical',
+			simulateTouch:SimulateTouch,
 			scrollContainer: true,
 			mousewheelControl: true,
+			onTouchMove:function(){
+				card.open=false;
+			},
+			onTouchEnd:function(){
+				setTimeout(function(){card.open=true;},500);
+			},			
 			scrollbar: {
 				container:$(this).find('.swiper-scrollbar')[0]
 			}
@@ -46,8 +105,14 @@ $(document).on('ready',function(){
 		$('#comparteList').toggle();
 		$('.filterPage').toggle();
 		
-		PAGE[0].reInit();
-		PAGE[0].resizeFix();
+		resetPage(0);
 	});
 
-});
+	$('#comparteList .card .image').on('click',card.show);
+
+	$('.header .right .icon').on('click',function(){
+		$('.header .right .menu').toggle();
+		$(this).toggleClass('active');
+	});
+
+}
